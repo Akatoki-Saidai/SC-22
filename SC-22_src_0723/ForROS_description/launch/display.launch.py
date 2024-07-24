ae990f6bb639d7493c3105ae65c6846ab395c501
sc22akatoki <akatoki.sc22@gmail.com>
2024-07-24 23:57:27 +0900

    a

--- SC-22_src_0723/ForROS_description/launch/display.launch.py
+++ SC-22_src_0723/ForROS_description/launch/display.launch.py
@@ -11,9 +11,8 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     share_dir = get_package_share_directory('ForROS_description')
 
-    xacro_file = os.path.join(share_dir, 'urdf', 'ForROS.xacro')
-    robot_description_config = xacro.process_file(xacro_file)
-    robot_urdf = robot_description_config.toxml()
+    urdf_file = os.path.join(share_dir, 'urdf', 'ForROS.urdf')
+    robot_urdf = urdf_file
 
     rviz_config_file = os.path.join(share_dir, 'config', 'display.rviz')
 
@@ -24,48 +23,6 @@ def generate_launch_description():
 
     show_gui = LaunchConfiguration('gui')
 
-    gnss_node = Node(
-        package='GNSS',
-        executable='gnss_node',
-        name='gnss',
-        namespace='gnss',                 
-        #remappings=[('chatter', 'chatter_app1')]    
-    )
-    bme280_node = Node(
-        package='bme280',
-        executable='bme280_node',
-        name='bme280',
-        namespace='bme280',                 
-        #remappings=[('chatter', 'chatter_app1')]    
-    )
-    bno055_node = Node(
-        package='bno055',
-        executable='bno055_node',
-        name='bno055',
-        namespace='bno055',                 
-        #remappings=[('chatter', 'chatter_app1')]    
-    )
-    camera_node = Node(
-        package='camera',
-        executable='camera_node',
-        name='camera',
-        namespace='camera',                 
-        #remappings=[('chatter', 'chatter_app1')]    
-    )
-    yolov10_node = Node(
-        package='yolov10',
-        executable='yolov10_node',
-        name='yolov10',
-        namespace='yolov10',                 
-        #remappings=[('chatter', 'chatter_app1')]    
-    )
-    yolov10_node = Node(
-        package='yolov10',
-        executable='yolov10_node',
-        name='yolov10',
-        namespace='yolov10',                 
-        #remappings=[('chatter', 'chatter_app1')]    
-    )
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
